7e4efcee595a5e03deb25327b71c4de9a251ac96
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-05-17 14:27:26 +0900

    [chore/#2] WriteActivity 생성

---
+++ app/src/main/java/org/sopt/seminar/WriteActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.seminar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
