ae1b54d4afdaaac1b0ec9c6c583c8bbd1b827a0b
chaeyeong <shimcy2010@gmail.com>
2022-05-18 16:26:54 +0900

    [feature/#7] 글 상세보기 UI

---
+++ app/src/main/java/org/sopt/seminar/presentation/read/ReadActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.seminar.presentation.read
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.seminar.R
+
+class ReadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
