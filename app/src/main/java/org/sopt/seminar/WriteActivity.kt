9cb6c8c3b973b52764356b30fbfce12dd2e4e975
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-05-20 22:00:23 +0900

    [feature/#2] write UI 구현

--- app/src/main/java/org/sopt/seminar/WriteActivity.kt
+++ app/src/main/java/org/sopt/seminar/WriteActivity.kt
@@ -1,11 +1,62 @@
 package org.sopt.seminar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
+import org.sopt.seminar.databinding.ActivityWriteBinding
+import org.sopt.seminar.presentation.home.PictureAdapter
+import org.sopt.seminar.presentation.home.PictureData
+import org.sopt.seminar.util.BaseActivity
 
-class WriteActivity : AppCompatActivity() {
+class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write) {
+    private lateinit var pictureAdapter: PictureAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+
+        initPictureAdapter()
+        changePriceColor()
+        backClickEvent()
     }
+
+    private fun changePriceColor() {
+        binding.etPrice.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length!! > 0) {
+                    binding.tvWon.setTextColor(
+                        ContextCompat.getColor(
+                            this@WriteActivity,
+                            R.color.carrot_black
+                        )
+                    )
+                } else {
+                    binding.tvWon.setTextColor(
+                        ContextCompat.getColor(
+                            this@WriteActivity,
+                            R.color.squaregray
+                        )
+                    )
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun initPictureAdapter() {
+        pictureAdapter = PictureAdapter()
+        binding.rvPicture.adapter = pictureAdapter
+    }
+
+    private fun backClickEvent() {
+        binding.btnBack.setOnClickListener() {
+            finish()
+        }
+    }
+
 }
