4343d828533ef4e762a5b16c4d8f1e509f95c61e
chaeyeong <shimcy2010@gmail.com>
2022-05-21 19:15:55 +0900

    [feature/#11] ClipData를 이용하여 이미지 다중선택 Uri 가져오기

--- app/src/main/java/org/sopt/seminar/WriteActivity.kt
+++ app/src/main/java/org/sopt/seminar/WriteActivity.kt
@@ -1,13 +1,27 @@
 package org.sopt.seminar
 
+import android.app.Activity
+import android.content.ClipData
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.google.android.material.internal.ContextUtils.getActivity
 import org.sopt.seminar.databinding.ActivityWriteBinding
 import org.sopt.seminar.presentation.home.PictureAdapter
 import org.sopt.seminar.presentation.home.PictureData
 import org.sopt.seminar.util.BaseActivity
+import java.util.jar.Manifest
 
 class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write) {
     private lateinit var pictureAdapter: PictureAdapter
@@ -18,7 +32,48 @@ class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write
         initPictureAdapter()
         changePriceColor()
         backClickEvent()
+
+
+        binding.layoutCamera.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.action = Intent.ACTION_PICK
+            requestActivity.launch(intent)
+        }
+
+
+
     }
+
+    private val requestActivity =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK && it.data?.data != null) { //갤러리 캡쳐 결과값
+                val clipData = it?.data?.clipData
+                val clipDataSize = clipData?.itemCount
+                if (clipData == null) { //이미지를 하나만 선택할 경우 clipData가 null이 올수 있음
+                    val selectedImageUri = it?.data?.data!!
+                    //TODO 얻어온 이미지 uri로 작업 진행
+                    Log.e("selectedImageUri", "$selectedImageUri")
+
+
+                } else {
+                    clipData.let { clipData ->
+                        for (i in 0 until clipDataSize!!) { //선택 한 사진수만큼 반복
+                            val selectedImageUri = clipData.getItemAt(i).uri
+                            val bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                            //pictureAdapter.pictureList.add(i, clipData.getItemAt(i).toString())
+                            pictureAdapter.pictureList.add(i, bitmap.toString())
+                            Log.e("selectedImageUri", "$selectedImageUri")
+                        }
+                    }
+                }
+                pictureAdapter.notifyDataSetChanged()
+                Log.e("adapter list", "${pictureAdapter.itemCount}")
+                Log.e("clip data size", "clipDataSize: $clipDataSize")
+            }
+        }
+
 
     private fun changePriceColor() {
         binding.etPrice.addTextChangedListener(object : TextWatcher {
--- app/src/main/java/org/sopt/seminar/presentation/home/PictureAdapter.kt
+++ app/src/main/java/org/sopt/seminar/presentation/home/PictureAdapter.kt
@@ -7,7 +7,8 @@ import com.bumptech.glide.Glide
 import org.sopt.seminar.databinding.ItemPictureListBinding
 
 class PictureAdapter : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() {
-    public val pictureList = mutableListOf<PictureData>()
+    //public val pictureList = mutableListOf<PictureData>()
+    val pictureList = mutableListOf<String>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
         val binding = ItemPictureListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -23,10 +24,11 @@ class PictureAdapter : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() 
     class PictureViewHolder(
         private val binding: ItemPictureListBinding
     ) : RecyclerView.ViewHolder(binding.root){
-        fun onBind(data: PictureData){
+        fun onBind(data: String){
             Glide.with(binding.root)
                 .load(data)
                 .into(binding.ivPicture)
         }
     }
+
 }
