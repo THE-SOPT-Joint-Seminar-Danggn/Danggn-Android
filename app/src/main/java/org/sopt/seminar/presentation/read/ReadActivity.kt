386d8624e97b9ffd59c682c98df757475e8034f0
chaeyeong <shimcy2010@gmail.com>
2022-05-21 14:23:28 +0900

    [feature/#7] viewpager, indicator 추가

--- app/src/main/java/org/sopt/seminar/presentation/read/ReadActivity.kt
+++ app/src/main/java/org/sopt/seminar/presentation/read/ReadActivity.kt
@@ -4,20 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.tabs.TabLayoutMediator
 import org.sopt.seminar.R
 import org.sopt.seminar.databinding.ActivityReadBinding
+import org.sopt.seminar.util.BaseActivity
 
-class ReadActivity : AppCompatActivity() {
+class ReadActivity : BaseActivity<ActivityReadBinding>(R.layout.activity_read) {
 
-    private lateinit var binding: ActivityReadBinding
 
     private val arrTextViewId = arrayListOf(R.id.tv_title, R.id.tv_1, R.id.tv_2, R.id.tv_3, R.id.tv_4, R.id.tv_5, R.id.tv_6)
 
+    private lateinit var imageViewPagerAdapter: ReadImageViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityReadBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val imageUrlList = listOf(
+            "https://cdn.newspenguin.com/news/photo/202006/1837_5156_215.jpg",
+            "https://cdnweb01.wikitree.co.kr/webdata/editor/202107/09/img_20210709133526_b7af1a66.webp"
+        )
+        imageViewPagerAdapter = ReadImageViewPagerAdapter(imageUrlList)
+        setUpViewPager()
+
+        TabLayoutMediator(binding.tlImages, binding.vpImages) {
+            tab, position ->
+        }.attach()
 
         val stateBottomSheetView = layoutInflater.inflate(R.layout.read_bottom_sheet, null)
         val stateBottomSheetDialog = BottomSheetDialog(this, R.style.DialogCustomTheme) // dialog에 sytle 추가
@@ -28,6 +42,16 @@ class ReadActivity : AppCompatActivity() {
         binding.clState.setOnClickListener {
             stateBottomSheetDialog.show()
         }
+    }
+
+    private fun setUpViewPager() {
+
+        binding.vpImages.adapter = imageViewPagerAdapter
+
+        binding.vpImages.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        val currentPageIndex = 1
+        binding.vpImages.currentItem = currentPageIndex
     }
 
     private fun setBottomSheetView(bottomSheetView: View, arr: Array<String>, dialog: BottomSheetDialog, spinner: TextView) {
@@ -41,5 +65,4 @@ class ReadActivity : AppCompatActivity() {
             }
         }
     }
-
 }
